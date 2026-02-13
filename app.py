435bdbed04bc622be5f8ef8d61c4cc53f375d049
mailinamit-debug <mailinamit@gmail.com>
2026-02-13 11:16:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="ML Classification App")
+
+st.title("Machine Learning Classification App")
+st.write("Upload a dataset and select a model to view predictions and metrics.")
